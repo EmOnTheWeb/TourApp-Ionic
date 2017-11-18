0c5cee303b243a0b57cf7bd8be7bf351ab072a96
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-11-18 16:35:53 +0000

    bring up to speed on walk object formatting

--- src/pages/walk/walk.ts
+++ src/pages/walk/walk.ts
@@ -14,7 +14,7 @@ export class WalkPage {
 	walkLandmarks; 
 	
 	constructor(public navCtrl: NavController, private storage: Storage, public navParams: NavParams, private walksService: WalksService) {
-		
+		this.storage.clear(); 
 	}
 
 	ngOnInit(): void {
@@ -32,15 +32,59 @@ export class WalkPage {
                 //retrieve from the api 
                 this.walksService.getDirections(walk.val)
                 .then((directions) => {
-                    this.walkDirections = directions; 
-                 
-                    this.storage.set(this.walk.name,directions).then(
+
+                    this.walkDirections = this.parseDirections(directions); 
+                  
+                    this.storage.set(this.walk.name,this.walkDirections).then(
                         (value) => console.log('directions stored'),
                         (error) => console.error('Error storing item', error)); 
                 });  
             }
         });  
         this.getLandmarks(walk);  
+    }
+
+    parseDirections(directions) {
+
+        let turnByTurn = directions['turn-by-turn'].json(); 
+        turnByTurn = JSON.parse(turnByTurn); 
+        let waypointsOnly = directions['waypoint-coordinates'];  
+           
+        let route = turnByTurn.routes[0]; 
+        let legs = route.legs; //a leg is a route between two waypoints     
+
+        //walk start and end coordinates
+        let startCoordinate = turnByTurn.waypoints[0].location.join(); 
+        let endCoordinate = turnByTurn.waypoints[turnByTurn.waypoints.length-1].location.join(); 
+
+        let legs = turnByTurn.routes[0].legs; 
+
+        for(let leg of legs) {
+            //remove properties you don't need 
+            delete leg.distance; 
+            delete leg.duration; 
+            delete leg.summary; 
+            delete leg.weight; 
+            //steps keep maneuver, location, type 
+            for(let step of leg.steps) {
+                     
+                delete step.distance; 
+                delete step.duration; 
+                // legs[i].steps[index].bearing = legs[i].steps[index].geometry;    
+                delete step.geometry; 
+                // delete legs[i].steps[index].intersections; 
+                delete step.mode; 
+                delete step.name; 
+                delete step.weight;  
+                step.instruction = step.maneuver.instruction; 
+                step.location = step.maneuver.location; 
+                step.type = step.maneuver.type;
+
+                delete step.maneuver; 
+                
+            }
+        }
+        return { beginning: startCoordinate, end: endCoordinate, legs: legs }; 
     }
 
     getLandmarks(walk:any) {
